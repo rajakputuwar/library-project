6133aa94fedfb41a5cd2aff1eb8fc3fe734d1d37
Rajak Putuwar <rajakputuwar07@gmail.com>
2023-06-27 22:39:04 +0545

    chanded login and register page

--- library-management-system/resources/views/auth/login.blade.php
+++ library-management-system/resources/views/auth/login.blade.php
@@ -1,129 +1,116 @@
 <!DOCTYPE html>
-
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+  <!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Login</title>
 
-    <meta name="description" content="" />
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('') }}design/assets/img/favicon/favicon.ico" />
+<!-- Favicon icon-->
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('') }}design/assets/images/favicon/favicon.ico">
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet" />
+<!-- Libs CSS -->
 
-    <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{ asset('') }}design/assets/vendor/fonts/boxicons.css" />
 
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('') }}design/assets/vendor/css/core.css"
-        class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('') }}design/assets/vendor/css/theme-default.css"
-        class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('') }}design/assets/css/demo.css" />
+<link href="{{ asset('') }}design/assets/libs/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+<link href="{{ asset('') }}design/assets/libs/dropzone/dist/dropzone.css"  rel="stylesheet">
+<link href="{{ asset('') }}design/assets/libs/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
+<link href="{{ asset('') }}design/assets/libs/prismjs/themes/prism-okaidia.css" rel="stylesheet">
 
-    <!-- Vendors CSS -->
-    <link rel="stylesheet"
-        href="{{ asset('') }}design/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="{{ asset('') }}design/assets/vendor/css/pages/page-auth.css" />
-    <!-- Helpers -->
-    <script src="{{ asset('') }}design/assets/vendor/js/helpers.js"></script>
-    <script src="{{ asset('') }}design/assets/js/config.js"></script>
+
+
+
+
+
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="{{ asset('') }}design/assets/css/theme.min.css">
+  <title>Log In</title>
 </head>
 
-<body>
-    <!-- Content -->
-
-    <div class="container-xxl">
-        <div class="authentication-wrapper authentication-basic container-p-y">
-            <div class="authentication-inner">
-                <!-- Register -->
-                <div class="card">
-                    <div class="card-body">
-
-                        <h4 class="mb-2">Library Management System</h4>
-
-                        <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="email" name="email"
-                                    placeholder="Enter your email" autofocus />
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="mb-3 form-password-toggle">
-                                <div class="d-flex justify-content-between">
-                                    <label class="form-label" for="password">Password</label>
-                                    <a href="auth-forgot-password-basic.html">
-                                        <small>Forgot Password?</small>
-                                    </a>
-                                </div>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control" name="password"
-                                        placeholder="Enter your password" aria-describedby="password" />
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                                </div>
-                            </div>
-
-                            <div class="mb-3">
-                                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                            </div>
-                        </form>
-
-                        <p class="text-center">
-                            <a href="{{ route('register') }}">
-                                <span>Create an account</span>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <!-- /Register -->
+<body class="bg-light">
+  <!-- container -->
+  <div class="container d-flex flex-column">
+    <div class="row align-items-center justify-content-center g-0
+        min-vh-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xxl-4 py-8 py-xl-0">
+        <!-- Card -->
+        <div class="card smooth-shadow-md">
+          <!-- Card body -->
+          <div class="card-body p-6">
+            <div class="mb-4">
+              <a href="{{ asset('') }}design/index.html"><img src="{{ asset('') }}design/assets/images/brand/logo/logo-primary.svg" class="mb-2" alt=""></a>
+              <p class="mb-6">Please enter your user information.</p>
             </div>
+            <!-- Form -->
+            <form>
+              <!-- Username -->
+              <div class="mb-3">
+                <label for="email" class="form-label">Username or email</label>
+                <input type="email" id="email" class="form-control" name="email" placeholder="Email address here" required="">
+              </div>
+              <!-- Password -->
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" id="password" class="form-control" name="password" placeholder="**************" required="">
+              </div>
+              <!-- Checkbox -->
+              <div class="d-lg-flex justify-content-between align-items-center
+                  mb-4">
+                <div class="form-check custom-checkbox">
+                  <input type="checkbox" class="form-check-input" id="rememberme">
+                  <label class="form-check-label" for="rememberme">Remember
+                      me</label>
+                </div>
+
+              </div>
+              <div>
+                <!-- Button -->
+                <div class="d-grid">
+                  <button type="submit" class="btn btn-primary">Sign
+                    in</button>
+                </div>
+
+                <div class="d-md-flex justify-content-between mt-4">
+                  <div class="mb-2 mb-md-0">
+                    <a href="sign-up.html" class="fs-5">Create An
+                        Account </a>
+                  </div>
+                  <div>
+                    <a href="forget-password.html" class="text-inherit
+                        fs-5">Forgot your password?</a>
+                  </div>
+
+                </div>
+              </div>
+
+
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+  <!-- Scripts -->
+  <!-- Libs JS -->
+<script src="{{ asset('') }}design/assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="{{ asset('') }}design/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('') }}design/assets/libs/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{ asset('') }}design/assets/libs/feather-icons/dist/feather.min.js"></script>
+<script src="{{ asset('') }}design/assets/libs/prismjs/prism.js"></script>
+<script src="{{ asset('') }}design/assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+<script src="{{ asset('') }}design/assets/libs/dropzone/dist/min/dropzone.min.js"></script>
+<script src="{{ asset('') }}design/assets/libs/prismjs/plugins/toolbar/prism-toolbar.min.js"></script>
+<script src="{{ asset('') }}design/assets/libs/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js"></script>
 
-    <!-- / Content -->
 
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-    <script src="{{ asset('') }}design/assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="{{ asset('') }}design/assets/vendor/libs/popper/popper.js"></script>
-    <script src="{{ asset('') }}design/assets/vendor/js/bootstrap.js"></script>
-    <script src="{{ asset('') }}design/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <script src="{{ asset('') }}design/assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
 
-    <!-- Vendors JS -->
-
-    <!-- Main JS -->
-    <script src="{{ asset('') }}design/assets/js/main.js"></script>
-
-    <!-- Page JS -->
-
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Theme JS -->
+<script src="{{ asset('') }}design/assets/js/theme.min.js"></script>
 </body>
 
 </html>
